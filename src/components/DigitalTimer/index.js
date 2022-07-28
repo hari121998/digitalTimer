805d0d418366e6174ed587fc0111e04697178326
hari charan Teja <haricharanpunna123@gmail.com>
2022-07-28 08:32:54 +0000

    changing

---
+++ src/components/DigitalTimer/index.js
@@ -0,0 +1,15 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class DigitalTimer extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Hi</h1>
+      </div>
+    )
+  }
+}
+
+export default DigitalTimer
