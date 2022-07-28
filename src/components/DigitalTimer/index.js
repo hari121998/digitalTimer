b6ffbff6716ebcde9489e5ee39c64d642aec8282
hari charan Teja <haricharanpunna123@gmail.com>
2022-07-28 13:34:59 +0000

    strucutre

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -3,10 +3,76 @@ import {Component} from 'react'
 import './index.css'
 
 class DigitalTimer extends Component {
+  state = {isStart: false}
+
+  startBtnClicked = () => {
+    this.setState(() => {
+      const {isStart} = this.state
+      return {isStart: !isStart}
+    })
+  }
+
   render() {
+    const {isStart} = this.state
+
     return (
       <div className="container">
-        <h1>Hi</h1>
+        <h1 className="heading">Digital Timer</h1>
+        <div className="set-timer-container">
+          <div className="bg-image-container">
+            <div className="start-time-container">
+              <p className="count-time">25:0</p>
+              <p className="time-status">{isStart ? 'Running' : 'Paused'}</p>
+            </div>
+          </div>
+          <div className="set-container">
+            <div className="reset-container">
+              <button
+                onClick={this.startBtnClicked}
+                type="button"
+                className="timer-btn"
+              >
+                {isStart ? (
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/pause-icon-img.png"
+                    alt="pause icon"
+                    className="play-pause-icon"
+                  />
+                ) : (
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/play-icon-img.png"
+                    alt="play icon"
+                    className="play-pause-icon"
+                  />
+                )}
+              </button>
+              <p className="status-name">{isStart ? 'Pause' : 'Start'}</p>
+              <button type="button" className="timer-btn">
+                <img
+                  src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png"
+                  alt="play icon"
+                  className="play-pause-icon"
+                />
+              </button>
+
+              <p className="status-name">Reset</p>
+            </div>
+            <div className="set-timer-limit-container">
+              <p className="timer-limit-heading">Set Timer limit</p>
+              <div className="increment-decrement">
+                <button type="button" className="inc-dec-btn">
+                  -
+                </button>
+                <button type="button" className="num-btn">
+                  25
+                </button>
+                <button type="button" className="inc-dec-btn">
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
